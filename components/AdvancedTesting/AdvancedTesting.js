f343c46cd01654318a99ad520df29bf85b53462a
glockstory <danila1025@gmail.com>
2022-07-13 16:27:21 +0300

    add advanced testing empty screen

---
+++ components/AdvancedTesting/AdvancedTesting.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AdvancedTesting() {
+  return (
+    <View>
+      <Text>AdvancedTesting</Text>
+    </View>
+  )
+}
