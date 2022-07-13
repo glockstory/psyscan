e12f7dc0cd935902de3fa564db1f674c930bd86f
glockstory <danila1025@gmail.com>
2022-07-13 17:09:51 +0300

    add LinearArea component

--- components/AdvancedTesting/AdvancedYourActivity.js
+++ components/AdvancedTesting/AdvancedYourActivity.js
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import LinearArea from "./LinearArea";
 
 export default function AdvancedYourActivity() {
   return (
-    <View>
-      <Text>AdvancedYourActivity</Text>
-    </View>
-  )
+    <LinearArea>
+      <View>
+        <Text>AdvancedYourActivity</Text>
+      </View>
+    </LinearArea>
+  );
 }
---
+++ components/AdvancedTesting/LinearArea.js
@@ -0,0 +1,24 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { styles } from "../../styles/style";
+
+export default function LinearArea() {
+  return (
+    <LinearGradient
+      style={[
+        styles.gradient,
+        { alignItems: "center", justifyContent: "center" },
+      ]}
+      colors={["#66B6D3", "#0F86AB", "#005980"]}
+      start={{
+        x: 0,
+        y: 0,
+      }}
+      end={{
+        x: 0,
+        y: 1,
+      }}
+    ></LinearGradient>
+  );
+}
