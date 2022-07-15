290cde5f1b5faae7ef039a3cead60103f48fce84
glockstory <danila1025@gmail.com>
2022-07-15 14:08:28 +0300

    add text analysis screen

--- components/AdvancedTesting/AdvancedTextAnalysis.js
+++ components/AdvancedTesting/AdvancedTextAnalysis.js
@@ -1,10 +1,21 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import LinearArea from "../Other/LinearArea";
+import AdvancedDescription from "./AdvancedDescription";
+import GradientButton from "../Buttons/GradientButton";
 
 export default function AdvancedTextAnalysis() {
   return (
-    <View>
-      <Text>AdvancedTextAnalysis</Text>
-    </View>
+    <LinearArea>
+      <AdvancedDescription>YOUR ACTIVITY</AdvancedDescription>
+      <View style={{ alignItems: "center" }}>
+        <Image
+          style={{ width: 124, height: 132 }}
+          source={require("../../assets/TextAnalysis.png")}
+        ></Image>
+        
+        <GradientButton title={"NEXT"}></GradientButton>
+      </View>
+    </LinearArea>
   );
 }
