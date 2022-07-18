7153d2c7651d2f08030c116ea3e274d78bfe527a
glockstory <danila1025@gmail.com>
2022-07-18 20:49:19 +0300

    add text writing screen

--- components/AdvancedTesting/AdvancedTextAnalysisWriting.js
+++ components/AdvancedTesting/AdvancedTextAnalysisWriting.js
@@ -1,17 +1,28 @@
-import { View, Text, Image, TextInput } from "react-native";
-import React, { useState } from "react";
+import { View, Text, Image, TextInput, SafeAreaView } from "react-native";
+import React from "react";
 import LinearArea from "../Other/LinearArea";
 import AdvancedDescription from "./AdvancedDescription";
+import { styles } from "../../styles/style";
 import GradientButton from "../Buttons/GradientButton";
-import PickerLanguage from "./Components/PickerLanguage";
-import { CheckBox } from "react-native-web";
-import PickerLangLevel from "./Components/PickerLangLevel";
 
 export default function AdvancedTextAnalysisWriting() {
   return (
     <LinearArea>
       <AdvancedDescription></AdvancedDescription>
-      <TextInput></TextInput>
+      <TextInput
+        editable
+        maxLength={300}
+        multiline={true}
+        numberOfLines={4}
+        style={{
+          backgroundColor: "#d7d7d7",
+          borderRadius: 10,
+          width: 300,
+          height: 500,
+          marginTop: -30
+        }}
+      ></TextInput>
+      <GradientButton title={'Save'}></GradientButton>
     </LinearArea>
   );
 }
