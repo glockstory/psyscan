48a9d54453d18fb00232dd936517b5f706684d13
glockstory <danila1025@gmail.com>
2022-07-18 21:04:43 +0300

    add video analysis screen

--- components/AdvancedTesting/AdvancedVideoAnalysis.js
+++ components/AdvancedTesting/AdvancedVideoAnalysis.js
@@ -1,10 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import LinearArea from "../Other/LinearArea";
+import AdvancedDescription from "./AdvancedDescription";
+import GradientButton from "../Buttons/GradientButton";
 
 export default function AdvancedVideoAnalysis() {
   return (
-    <View>
-      <Text>AdvancedVideoAnalysis</Text>
-    </View>
+    <LinearArea>
+      <AdvancedDescription>FACE SCAN</AdvancedDescription>
+      <Image
+        style={{ width: 200, height: 200, marginBottom: 10, display: "block" }}
+        source={require("../../assets/videoanalysis.png")}
+      ></Image>
+      <Text
+        style={{
+          color: "white",
+          fontSize: 16,
+          textAlign: "center",
+          marginBottom: 5,
+          width: 300,
+        }}
+      >
+        Explain what is going to happen, why we need to scan. Give instuctions
+        to the user.
+      </Text>
+      <GradientButton title={'START THE SCAN'}></GradientButton>
+    </LinearArea>
   );
 }
