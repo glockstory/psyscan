83b98376ac8227ef33d2e7f1b2db930d3eccd444
glockstory <danila1025@gmail.com>
2022-07-18 21:01:02 +0300

    add facescan screen

--- components/AdvancedTesting/AdvancedFaceScan.js
+++ components/AdvancedTesting/AdvancedFaceScan.js
@@ -1,10 +1,30 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import LinearArea from "../Other/LinearArea";
+import AdvancedDescription from "./AdvancedDescription";
+import GradientButton from "../Buttons/GradientButton";
 
 export default function AdvancedFaceScan() {
   return (
-    <View>
-      <Text>AdvancedFaceScan</Text>
-    </View>
+    <LinearArea>
+      <AdvancedDescription>FACE SCAN</AdvancedDescription>
+      <Image
+        style={{ width: 250, height: 335, marginBottom: 10, display: "block" }}
+        source={require("../../assets/facescan.png")}
+      ></Image>
+      <Text
+        style={{
+          color: "white",
+          fontSize: 16,
+          textAlign: "center",
+          marginBottom: 5,
+          width: 300,
+        }}
+      >
+        Explain what is going to happen, why we need to scan. Give instuctions
+        to the user.
+      </Text>
+      <GradientButton title={'START THE SCAN'}></GradientButton>
+    </LinearArea>
   );
 }
