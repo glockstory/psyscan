09ad5a373e5d845aa5a9f220ece26e9dfcf7d296
glockstory <danila1025@gmail.com>
2022-07-29 13:18:03 +0300

    add audio recording

--- components/AdvancedTesting/AdvancedAudioAnalysis.js
+++ components/AdvancedTesting/AdvancedAudioAnalysis.js
@@ -1,11 +1,41 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import React from "react";
 import LinearArea from "../Other/LinearArea";
 import AdvancedDescription from "./AdvancedDescription";
 import GradientButton from "../Buttons/GradientButton";
 import Slider from "@react-native-community/slider";
+import { Audio } from "expo-av";
 
 export default function AdvancedAudioAnalysis() {
+  const [recording, setRecording] = React.useState();
+
+  async function startRecording() {
+    try {
+      console.log("Requesting permissions..");
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+      console.log("Starting recording..");
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY
+      );
+      setRecording(recording);
+      console.log("Recording started");
+    } catch (err) {
+      console.error("Failed to start recording", err);
+    }
+  }
+
+  async function stopRecording() {
+    console.log("Stopping recording..");
+    setRecording(undefined);
+    await recording.stopAndUnloadAsync();
+    const uri = recording.getURI();
+    console.log("Recording stopped and stored at", uri);
+    alert('saved')
+  }
   return (
     <LinearArea>
       <AdvancedDescription>AUDIO ANALYSIS</AdvancedDescription>
@@ -31,8 +61,20 @@ export default function AdvancedAudioAnalysis() {
           Now you will be asked to do the audio recording, follow the
           instruction to get through the test
         </Text>
-        <GradientButton title={"START RECORDING"}></GradientButton>
+        <GradientButton
+          title={recording ? "Stop Recording" : "Start Recording"}
+          onPress={recording ? stopRecording : startRecording}
+        ></GradientButton>
       </View>
     </LinearArea>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 10,
+  },
+});
--- components/AdvancedTesting/AdvancedFaceScan.js
+++ components/AdvancedTesting/AdvancedFaceScan.js
@@ -4,12 +4,12 @@ import LinearArea from "../Other/LinearArea";
 import AdvancedDescription from "./AdvancedDescription";
 import GradientButton from "../Buttons/GradientButton";
 
-export default function AdvancedFaceScan({navigation}) {
+export default function AdvancedFaceScan({ navigation }) {
   return (
     <LinearArea>
       <AdvancedDescription>FACE SCAN</AdvancedDescription>
       <Image
-        style={{ width: 250, height: 335, marginBottom: 10}}
+        style={{ width: 250, height: 335, marginBottom: 10 }}
         source={require("../../assets/facescan.png")}
       ></Image>
       <Text
@@ -24,7 +24,10 @@ export default function AdvancedFaceScan({navigation}) {
         Explain what is going to happen, why we need to scan. Give instuctions
         to the user.
       </Text>
-      <GradientButton title={'START THE SCAN'} onPress={() => navigation.navigate("AdvancedFaceScanCamera")}></GradientButton>
+      <GradientButton
+        title={"START THE SCAN"}
+        onPress={() => navigation.navigate("AdvancedFaceScanCamera")}
+      ></GradientButton>
     </LinearArea>
   );
 }
